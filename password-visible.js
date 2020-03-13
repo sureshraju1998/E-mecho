19dff2280e23e38a7e59ec7c097e452ee2e5c3ca
v suresh <61861864+sureshraju1998@users.noreply.github.com>
2020-03-13 09:35:24 +0530

    Create password-visible.js

---
+++ password-visible.js
@@ -0,0 +1,50 @@
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("icon1").className = "fa fa-eye-slash";
+    } else {
+      x.type = "password";
+      document.getElementById("icon1").className = "fa fa-eye";
+    }
+  }
+function myFunction2() {
+    var x = document.getElementById("myInput2");
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("icon2").className = "fa fa-eye-slash";
+    } else {
+      x.type = "password";
+      document.getElementById("icon2").className = "fa fa-eye";
+    }
+  }
+function myFunction3() {
+    var x = document.getElementById("myInput3");
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("icon3").className = "fa fa-eye-slash";
+    } else {
+      x.type = "password";
+      document.getElementById("icon3").className = "fa fa-eye";
+    }
+  }
+function myFunction4() {
+    var x = document.getElementById("myInput4");
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("icon4").className = "fa fa-eye-slash";
+    } else {
+      x.type = "password";
+      document.getElementById("icon4").className = "fa fa-eye";
+    }
+  }
+function myFunction5() {
+    var x = document.getElementById("myInput5");
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("icon5").className = "fa fa-eye-slash";
+    } else {
+      x.type = "password";
+      document.getElementById("icon5").className = "fa fa-eye";
+    }
+  }
